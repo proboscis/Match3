13838b73d2f64a147ce921d986bfc59c18712ec5
masui kento <nameissoap@gmail.com>
2013-10-25 17:16:33 +0900

    removed invalid statements.

--- common/src/test/scala/JacksonTest.scala
+++ common/src/test/scala/JacksonTest.scala
@@ -11,17 +11,20 @@ object JacksonTest extends Properties("jackson") {
     (i:Int)=>{
       import com.codahale.jerkson.Json._
 
+      import scala.util.Try
+      val a = Try("aa")
       // Parse JSON arrays
       parse[List[Int]]("[1,2,3]") //=> List(1,2,3)
 
       // Parse JSON objects
       parse[Map[String, Int]]("""{"one":1,"two":2}""") //=> Map("one"->1,"two"->2)
-
+/*
       // Parse JSON objects as case classes
-      // (Parsing case classes isn't supported in the REPL.)
+      // (Parsing case classes isn't supported in the REPL.)!!
       case class Person(id: Long, name: String)
       val p = parse[Person]("""{"id":1,"name":"Coda"}""") //=> Person(1,"Coda")
       println(p)
+      */
       /*
       // Parse streaming arrays of things
       for (person <- stream[Person](System.in)) {
