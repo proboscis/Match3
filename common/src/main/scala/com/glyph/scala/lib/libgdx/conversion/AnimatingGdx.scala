9d95607059db141d285dfa2b80835311a65ec84c
kento masui <nameissoap@gmail.com>
2013-12-08 20:27:14 +0900

    aapt error on vaio tap

--- common/src/main/scala/com/glyph/scala/lib/libgdx/conversion/AnimatingGdx.scala
+++ common/src/main/scala/com/glyph/scala/lib/libgdx/conversion/AnimatingGdx.scala
@@ -2,7 +2,7 @@ package com.glyph.scala.lib.libgdx.conversion
 
 import com.glyph.scala.lib.util.animator.{AnimatedFloat2, Animating}
 import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.graphics.g2d.{SpriteBatch, Sprite}
+import com.badlogic.gdx.graphics.g2d.{TextureRegion, SpriteBatch, Sprite}
 import com.glyph.scala.lib.libgdx.actor.{SpriteActor, SBDrawable}
 import com.badlogic.gdx.scenes.scene2d.Actor
 
@@ -14,7 +14,6 @@ object AnimatingGdx {
   object AnimatingVector2X extends Animating[Vector2]{
     def get: Vector2 = ???
   }*/
-  //TODO これ、ランタイムでリフレクションを使ってるぞ・・・・！？オソロシヤ
   type Animated ={
     def getX():Float
     def getY():Float
@@ -22,6 +21,7 @@ object AnimatingGdx {
     def setY(y:Float):Unit
   }
 
+  //this uses reflection which does not work on android
   def generateAdapter[T<:Animated]:AnimatedFloat2[T] = new AnimatedFloat2[T] {
     def setY(tgt: T)(y: Float): Unit = tgt.setY(y)
 
@@ -32,8 +32,31 @@ object AnimatingGdx {
     def getX(tgt: T): Float =tgt.getX()
   }
 
+  def animatedActor[T<:Actor]:AnimatedFloat2[T] =  new AnimatedFloat2[T] {
+    def setY(tgt: T)(y: Float): Unit = tgt.setY(y)
+
+    def getY(tgt: T): Float = tgt.getY
+
+    def setX(tgt: T)(x: Float): Unit = tgt.setX(x)
+
+    def getX(tgt: T): Float =tgt.getX
+  }
+  def animatedSprite[T<:Sprite]:AnimatedFloat2[T] = new AnimatedFloat2[T] {
+    def setY(tgt: T)(y: Float): Unit = tgt.setY(y)
+
+    def getY(tgt: T): Float = tgt.getY
+
+    def setX(tgt: T)(x: Float): Unit = tgt.setX(x)
+
+    def getX(tgt: T): Float =tgt.getX
+  }
+
+
+
   //TODO you need to keep these classes from proguard...
-  implicit val Animated2Sprite = generateAdapter[Sprite]
-  implicit val AnimatedActor = generateAdapter[Actor]
-  implicit val AnimatedSpriteActor = generateAdapter[SpriteActor]
+  //i want the implicit macro for these type classes.
+  implicit val Animated2Sprite = animatedSprite[Sprite]
+  implicit val AnimatedActor = animatedActor[Actor]
+  implicit val AnimatedSpriteActor = animatedActor[SpriteActor]
+
 }
