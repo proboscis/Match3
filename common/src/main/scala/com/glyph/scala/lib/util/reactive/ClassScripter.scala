15d0dbb3cb71894651dac9739f9b6ca3318f3709
Kento Masui <nameissoap@gmail.com>
2014-01-24 21:52:15 +0900

    moved libraries into desktop, moved proguard options into root dir as text file

--- common/src/main/scala/com/glyph/scala/lib/util/reactive/ClassScripter.scala
+++ common/src/main/scala/com/glyph/scala/lib/util/reactive/ClassScripter.scala
@@ -1,84 +1,14 @@
 package com.glyph.scala.lib.util.reactive
 
-import com.glyph.scala.lib.util.Logging
-import scala.reflect.io.{Directory, Path}
-import java.io.File
-import com.googlecode.scalascriptengine.{SourcePath, Config, ScalaScriptEngine}
 import scala.util.{Failure, Success, Try}
 import scala.reflect.ClassTag
 import com.badlogic.gdx.{Application, Gdx}
 import com.glyph.scala.lib.libgdx.GdxUtil
+import com.badlogic.gdx.Application.ApplicationType
 
-/**
- * @author glyph
- */
-class ClassScripter(srcDir: String, outDir: String, classDir: String) extends SourceChecker(srcDir, outDir) with Logging {
-  val sourcePath = outDir
-  val sourceDir = new File(sourcePath)
-  Directory.apply(Path(sourcePath)).deleteRecursively()
-  new File(classDir).mkdirs()
-  val sse = new ScalaScriptEngine(Config(SourcePath(sourceDir, new File(classDir)) :: Nil))
-  sse.deleteAllClassesInOutputDirectory()
-  try {
-    sse.refresh
-  } catch {
-    case e: Throwable => e.printStackTrace()
-  }
-
-  override def onFileChange(file: File): Unit = {
-    super.onFileChange(file)
-    log("fileChange")
-    try {
-      sse.refresh
-    } catch {
-      case e: Throwable => e.printStackTrace()
-    }
-  }
-
-  def try2Opt[T](t: Try[T]): Option[T] = t match {
-    case Success(s) => Some(s)
-    case Failure(e: IllegalStateException) => None
-    case Failure(f) => f.printStackTrace(); None
-  }
-
-  def getClass[I, T <: I : ClassTag]: Varying[Option[Class[I]]] = {
-    log("getClass")
-    super.get[T].map {
-      file => {
-        log(("mapFileToClass", file.getAbsolutePath))
-        val tag = implicitly[ClassTag[T]]
-        val canon = tag.runtimeClass.getCanonicalName
-        val simple = tag.runtimeClass.getSimpleName
-        val replaced = canon.replace(simple, prefix + simple)
-        log("replaced", replaced)
-        val opt = try2Opt(Try(sse.get[I](replaced)))
-        if (opt.isDefined) opt
-        else {
-          try2Opt(Try(Class.forName(implicitly[ClassTag[T]].runtimeClass.getCanonicalName).asInstanceOf[Class[I]]))
-        }
-      }
-    }
-  }
-
-  def getClass[I](clsName: String): Varying[Option[Class[I]]] = {
-    super.get[I](clsName).map {
-      file => {
-        log(("mapFileToClass", file.getAbsolutePath))
-        val cls = Class.forName(clsName)
-        val canon = cls.getCanonicalName
-        val simple = cls.getSimpleName
-        val replaced = canon.replace(simple, prefix + simple)
-        log("replaced", replaced)
-        val opt = try2Opt(Try(sse.get[I](replaced)))
-        if (opt.isDefined) opt
-        else {
-          try2Opt(Try(cls.asInstanceOf[Class[I]]))
-        }
-      }
-    }
-  }
-
-  start()
+trait VClass {
+  def getClass[I, T <: I : ClassTag]: Varying[Option[Class[I]]]
+  def getClass[I](clsName: String): Varying[Option[Class[I]]]
 }
 
 object VClass {
@@ -88,11 +18,14 @@ object VClass {
   var srcDir = "./src/main/scala"
   var mirrorDir = "./.changed"
   var mirrorClassDirName = ".classes"
+
+  lazy val desktopImpl = Class.forName("com.glyph.scala.lib.util.reactive.ClassScripter").getConstructor(classOf[String], classOf[String], classOf[String]).newInstance(srcDir, mirrorDir, mirrorDir + "/" + mirrorClassDirName).asInstanceOf[VClass]
+
   lazy val scripter = if (Gdx.app == null) {
-    new ClassScripter(srcDir, mirrorDir, mirrorDir + "/" + mirrorClassDirName)
+    throw new RuntimeException("cannot create VClass the application is created")
   } else Gdx.app.getType match {
     case Android => throw new RuntimeException("cannot use VClass on android!")
-    case Desktop => new ClassScripter(srcDir, mirrorDir, mirrorDir + "/" + mirrorClassDirName)
+    case Desktop => desktopImpl
     case _ => throw new RuntimeException("VClass is not supported on this device")
   }
 
@@ -119,10 +52,10 @@ object VClass {
     if (Gdx.app == null) desktop
     else {
       Gdx.app.getType match {
-        case Android => nop
-        case Desktop => desktop
-        case iOS => nop
-        case WebGL => nop
+        case ApplicationType.Android => nop
+        case ApplicationType.Desktop => desktop
+        case ApplicationType.iOS => nop
+        case ApplicationType.WebGL => nop
       }
     }
   }
--- project/build.scala
+++ project/build.scala
@@ -5,30 +5,26 @@ import org.scalasbt.androidplugin._
 import org.scalasbt.androidplugin.AndroidKeys._
 import sbtassembly.Plugin._
 import AssemblyKeys._
-object Settings {
+object Constants{
   val scalazVersion = "7.0.4"
   val liftVersion = "2.5"
-  val sversion = "2.10.3"
+  val sVersion = "2.10.3"
+  val gdxVersion = "1.0-SNAPSHOT"
+}
+object Settings {
+  import Constants._
   lazy val common = Defaults.defaultSettings ++ Seq (
-    version := "0.1",
-    scalaVersion := sversion,
+    version := "0.2",
+    scalaVersion := sVersion,
     resolvers += "spray" at "http://repo.spray.io/"//this is required to use spray
     ,
     {
       libraryDependencies ++=  Seq(
-        "com.github.scopt" %% "scopt" % "3.1.0", 
         "org.scalaz" %% "scalaz-core" % scalazVersion,
         "org.scalaz" %% "scalaz-effect" % scalazVersion,
         "org.scalaz" %% "scalaz-typelevel" % scalazVersion,
-        "net.liftweb" %% "lift-json" % liftVersion,
-        "io.spray" %%  "spray-json" % "1.2.5",
-        "com.googlecode.scalascriptengine" % "scalascriptengine" % ("1.3.7-"+sversion),
-        "org.scala-lang" % "scala-compiler" % sversion,
-        //"net.liftweb" %% "lift-json-scalaz" % liftVersion,
-        "org.scala-lang" % "scala-reflect" % sversion,
-        "org.scalacheck" %% "scalacheck" % "1.10.1" % "test")
-        //libraryDependencies += "org.scala-lang" % "scala-library" % "2.10.1"
-    },
+        "io.spray" %%  "spray-json" % "1.2.5")
+      },
     //scalacOptions ++=Seq("-optimize"),
     //scalacOptions ++=Seq("-Xprint:lambdalift"),
     scalacOptions ++=Seq("-feature"),
@@ -40,115 +36,22 @@ object Settings {
     addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise" % "2.0.0-SNAPSHOT" cross CrossVersion.full)
     ,
     updateLibgdxTask
-   )
+    )
   lazy val desktop = Settings.common ++  Seq (
     fork in Compile := true
-  ) 
+    ) 
   lazy val android = Settings.common ++
-    AndroidProject.androidSettings ++
-    AndroidMarketPublish.settings ++ Seq (
-      platformName in Android := "android-10",
-      keyalias in Android := "change-me",
-      mainAssetsPath in Android := file("common/src/main/resources"),
-      unmanagedBase <<= baseDirectory( _ /"src/main/libs" ),
-      proguardOption in Android := 
-      "-keep class com.badlogic.gdx.backends.android.** { *; }"+
-      "-keep class scala.collection.SeqLike {public protected *;}"+
-      "-keep class org.mozilla.** {*;}"+
-      "-keep class com.esotericsoftware.** {*;}"+
-      """
--keepclassmembers enum * {
-    public static **[] values();
-    public static ** valueOf(java.lang.String);
-}
--keep class com.badlogic.gdx.scenes.scene2d.ui.**
--keep class com.badlogic.gdx.graphics.g2d.Sprite
--keep class com.badlogic.gdx.scenes.scene2d.Actor
--keep class com.glyph.scala.lib.libgdx.actor.SpriteActor
--keep class com.badlogic.gdx.physics.box2d.** {*;}
--keep class com.glyph.scala.test.**
--keep class scala.Function0 
--keep class scala.Function1
--keep class scala.Function2
-
--optimizationpasses 5
--dontusemixedcaseclassnames
--dontskipnonpubliclibraryclasses
--dontpreverify
--verbose
--optimizations !code/simplification/arithmetic,!field/*,!class/merging/*
-
--renamesourcefileattribute SourceFile
--keepattributes SourceFile,LineNumberTable
-
-# http://stackoverflow.com/questions/4525661/android-proguard-cant-find-dynamically-referenced-class-javax-swing
--dontwarn java.awt.**
--dontnote java.awt.**
--dontwarn com.badlogic.gdx.jnigen.**
--dontwarn com.badlogic.**
--dontnote com.badlogic.**
-
--libraryjars /libs/gdx-backend-android.jar
--libraryjars ../Pogopainter/libs/gdx.jar
-
--keep public class * extends android.app.Activity
--keep public class * extends android.app.Application
--keep public class * extends android.app.Service
--keep public class * extends android.content.BroadcastReceiver
--keep public class * extends android.content.ContentProvider
--keep public class * extends android.app.backup.BackupAgentHelper
--keep public class * extends android.preference.Preference
-#-keep public class com.android.vending.licensing.ILicensingService
--keep class com.badlogic.**
--keep class com.badlogic.backends.**
--keep class * implements com.badlogic.gdx.utils.Json*
--keep class com.google.**
--keep class java.lang.reflect.**
--keep class scala.collection.**
-
--keep class com.glyph.scala.lib.libgdx.screen.**
-
-
-# https://ofdev.zendesk.com/entries/20461397-android-pointless-proguard-cfg
--keep class com.openfeint** { <methods>; }
-
--keepnames class * implements java.io.Serializable
-
--keepclassmembers class * implements java.io.Serializable {
-    static final long serialVersionUID;
-    private static final java.io.ObjectStreamField[] serialPersistentFields;
-    !static !transient <fields>;
-    private void writeObject(java.io.ObjectOutputStream);
-    private void readObject(java.io.ObjectInputStream);
-    java.lang.Object writeReplace();
-    java.lang.Object readResolve();
-}
-
--keepclasseswithmembernames class * {
-    native <methods>;
-}
-
--keepclasseswithmembers class * {
-    public <init>(android.content.Context, android.util.AttributeSet);
-}
-
--keepclasseswithmembers class * {
-    public <init>(android.content.Context, android.util.AttributeSet, int);
-}
-
--keepclassmembers class * extends android.app.Activity {
-   public void *(android.view.View);
-}
-
--keep class * implements android.os.Parcelable {
-  public static final android.os.Parcelable$Creator *;
-}
-
--keep class com.android.vending.billing.**
-
--keep enum * {    public static **[] values();    public static ** valueOf(java.lang.String); }
-
-      """
+  AndroidProject.androidSettings ++
+  AndroidMarketPublish.settings ++ Seq (
+    platformName in Android := "android-10",
+    keyalias in Android := "change-me",
+    mainAssetsPath in Android := file("common/src/main/resources"),
+    unmanagedBase <<= baseDirectory( _ /"src/main/libs" ),
+    proguardOption in Android := {
+      import scala.io.Source
+      val options = Source.fromFile("./proguardOptions.txt").getLines.mkString(" ")
+      options
+    }
     )
   val updateLibgdx = TaskKey[Unit]("update-gdx", "Updates libgdx")
 
@@ -157,7 +60,7 @@ object Settings {
     import java.io._
     import java.net.URL
     import java.util.regex.Pattern
-    
+
     // Declare names
     val baseUrl = "http://libgdx.badlogicgames.com/nightlies"
     val gdxName = "libgdx-nightly-latest"
@@ -204,28 +107,36 @@ object Settings {
 }
 
 object LibgdxBuild extends Build {
-
+  import Constants._
   lazy val macros = Project(
     "macros",
     file("macros"),
     settings = Settings.common ++ Seq(libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _))
-  )
+    )
 
   val common = Project (
     "common",
     file("common"),
     settings = Settings.common
-  ) dependsOn macros
+    ) dependsOn macros
 
   lazy val desktop = Project (
     "desktop",
     file("desktop"),
-    settings = Settings.desktop ++ assemblySettings 
-  ) dependsOn common 
+    settings = Settings.desktop ++ assemblySettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "com.googlecode.scalascriptengine" % "scalascriptengine" % ("1.3.7-"+sVersion),
+        "org.scala-lang" % "scala-compiler" % sVersion,
+        "org.scala-lang" % "scala-reflect" % sVersion,
+        "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
+        "com.github.scopt" %% "scopt" % "3.1.0"
+        )
+      )
+    ) dependsOn common 
 
   lazy val android = Project (
     "android",
     file("android"),
     settings = Settings.android
-  ) dependsOn common
+    ) dependsOn common
 }
