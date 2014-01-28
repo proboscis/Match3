c7c6070896d79d7aabd076de28fbd58e725d3cdd
kento masui <nameissoap@gmail.com>
2014-01-28 18:34:10 +0900

    backup

--- common/src/main/scala/com/glyph/scala/test/Transition.scala
+++ common/src/main/scala/com/glyph/scala/test/Transition.scala
@@ -1,7 +1,8 @@
 package com.glyph.scala.test
 
-import scala.annotation.tailrec
 import com.badlogic.gdx.math.MathUtils
+import scala.collection.mutable
+import scalax.collection.edge
 
 /**
  * @author proboscis
@@ -12,14 +13,29 @@ trait Transition[+T] {
 
 object Transitions{
   def toGraph[G](root:G)(implicit ev:G => Seq[G]) = {
-    def rec(r:G,set:Set[(G,G)]):Set[(G,G)]={
-      val dsts = ev(r)
-      val edges = dsts.map(r->_)
-      if(edges.forall(set.contains)) set else{
-        dsts.flatMap(dst => rec(dst,set ++ edges)).toSet
+    val visited = mutable.HashMap[G,Boolean]() withDefault (_=>false)
+    var edges:Seq[(G,G)] = Seq()
+    val stack:collection.mutable.Stack[G] = mutable.Stack[G](root)
+    while(!stack.isEmpty){
+      val node = stack.pop()
+      if(!visited(node)){
+        println(node)
+        val dsts = ev(node)
+        edges ++= dsts map (node->_)
+        visited.put(node,true)
+        stack.pushAll(dsts)
       }
     }
+    edges
+    /*
+    def rec(r:G,set:Set[(G,G)]):Set[(G,G)]={
+      val edges = ev(r).map(r->_).toSet
+      val newEdges = edges.filterNot(set.contains)
+      val newSet = set ++ newEdges
+      set ++ newEdges.flatMap(dst=>rec(dst._2,newSet))
+    }
     rec(root,Set())
+    */
   }
   import scalax.collection.Graph
   import scalax.collection.GraphPredef._
@@ -30,7 +46,7 @@ object Transitions{
     }
     val g = 1 to 100 map (i => new G(i.toString))
     g foreach{
-      n => n.destinations = 1 to 1 map (_=>MathUtils.random(1,99)) map g
+      n => n.destinations = 1 to 3 map (_=>MathUtils.random(1,99)) map g
     }
     val graph = toGraph(g(0))(_.destinations)
     println(graph)
